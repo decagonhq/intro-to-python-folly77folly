cb1e700f179e20910faabac5d7d928817954ba93
aqim <folly77folly@gmail.com>
2020-01-10 11:11:52 +0100

    stated args and kargs

--- oneoffs.py
+++ oneoffs.py
@@ -8,3 +8,13 @@ def divide(numb1,numb2,floor=True):
             print (first_Number/second_Number)        
     except TypeError as e:
         print(e)
+
+
+def last_song(*song_title,**date_of_release):
+  pass
+
+
+
+songs_title=['a','b','c','d']
+date_of_release={'year': "2020", 'month': "01", 'day': "01"}
+last_song(songs_title)
